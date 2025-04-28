5ea20eac064278b086c03a6b1d8b12abe7578eb5
Iyad ELwy <iyadelwy1@gmail.com>
2025-04-28 18:09:32 +0200

    feat: remove move cli message

--- web/js/customTerminal.js
+++ web/js/customTerminal.js
@@ -32,7 +32,7 @@
     ▐▙▄▄▖█                ▐▌ ▐▌█▄▄█          ▐▌         ▝▚▄▟▌
                                                                  
                                                                                                                            
-You are currently in a Live Terminal Session running in multiple K8s Pods.\n\nThis environment was carefully designed to ensure robustness and security by running it on my own self-hosted Kubernetes Multi-Node cluster, running in my kitchen :)\n\nTo know more about how this environment was built check out the "Portfolio Projects" section bellow to access the project\'s GitHub.\n\nI also wrote a small Movie data CLI Tool and installed it into the pod so you don't get bored here.\nTo get started type "movies --help"\n\nTry looking around with ls\n\nP.S. Don't worry about breaking anything...Thanks to K8s, you can't.\n\n\n`,
+You are currently in a Live Terminal Session running in multiple K8s Pods.\n\nThis environment was carefully designed to ensure robustness and security by running it on my own self-hosted Kubernetes Multi-Node cluster, running in my kitchen :)\n\nTo know more about how this environment was built check out the "Portfolio Projects" section bellow to access the project\'s GitHub.\n\nTry looking around with ls\n\nP.S. Don't worry about breaking anything...Thanks to K8s, you can't.\n\n\n`,
         prompt: '> '
     });
 
