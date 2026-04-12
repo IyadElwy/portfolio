73ee3c5d51b1878112c9919c0ab407c894b01d4c
iyadelwy <iyadelwy1@gmail.com>
2026-04-12 14:50:18 +0200

    feat: remove restrictive bash

--- vm-container/api/main.py
+++ vm-container/api/main.py
@@ -18,19 +18,13 @@ async def cmd(command_body: CommandBody):
             command_body.command,
             capture_output=True,
             shell=True,
-            executable="/home/narrator/bin/rbash",
-            user="narrator",
-            group="narrator",
-            env={
-                "PATH": "/home/narrator/bin",  # Restrict PATH to rbash environment
-                "HOME": "/home/narrator",  # Set home for the user
-            },
+            executable="/bin/bash",
             timeout=15,
         )
         encoded_result = result.stdout.decode("utf-8") + result.stderr.decode("utf-8")
         return {"result": encoded_result}
     except Exception:
-        raise {"result": "Network Error: Your command did not reach the host machine."}
+        return {"result": "Network Error: Your command did not reach the host machine."}
 
 
 @app.get("/health")
@@ -40,16 +34,10 @@ async def health():
             "ls",
             capture_output=True,
             shell=True,
-            executable="/home/narrator/bin/rbash",
-            user="narrator",
-            group="narrator",
-            env={
-                "PATH": "/home/narrator/bin",
-                "HOME": "/home/narrator",
-            },
+            executable="/bin/bash",
             timeout=15,
         )
         encoded_result = result.stdout.decode("utf-8") + result.stderr.decode("utf-8")
         return {"result": encoded_result}
     except Exception:
-        raise {"result": "Network Error: Your command did not reach the host machine."}
+        return {"result": "Network Error: Your command did not reach the host machine."}
